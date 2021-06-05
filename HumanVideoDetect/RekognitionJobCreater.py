8ec5c8204125015e110b5258ad9ecebe573b2182
EC2 Default User <ec2-user@ip-172-31-30-190.us-east-2.compute.internal>
2021-06-05 02:55:34 +0000

    Line 97-118: Added SQS message fetcher - Errors with response parsing - JSON/Dict parse issues

--- HumanVideoDetect/RekognitionJobCreater.py
+++ HumanVideoDetect/RekognitionJobCreater.py
@@ -63,11 +63,28 @@ def S3Exist(InputBucket, InputKey):
         return ('False')
 
 # Work on Below to combine below function to work with FrameExtracter and omit JSON file output and reads. 
-def ResultsPaginater(RekognitionJobID, RekognitionNextToken):
+def RekognitionResultsPublisher(RekognitionJobID, RekognitionNextToken, SqsUrl):
+  sqs = boto3.client('sqs')
   Done = False
   count = 1
   while Done == False: 
       JobStatusCheck = JobResultsFetcher(RekognitionJobID, RekognitionNextToken)
+      SendMessage = sqs.send_message(
+        QueueUrl=SqsUrl,
+        MessageBody=str(JobStatusCheck),
+        MessageAttributes={
+            'JobId': {
+            'StringValue': str(RekognitionJobID),
+            'DataType': 'String'
+                },
+            'Sequence':{
+                'StringValue': str(count),
+                'DataType': 'Number'
+            }
+            }
+        )
+        
+      #print(SendMessage)
       with open(str(count) + 'data.json', 'w', encoding='utf-8') as f:
         json.dump(JobStatusCheck, f, ensure_ascii=False, indent=4)
         count += 1
@@ -77,7 +94,30 @@ def ResultsPaginater(RekognitionJobID, RekognitionNextToken):
           Done = True
           return Done
 
+def GetSqsMessages(SqsUrl, RekognitionJobID):
+    sqs = boto3.client('sqs')
+    queueAttributes = sqs.get_queue_attributes(
+        QueueUrl=SqsUrl,
+        AttributeNames=['All']
+        )
+        #print(queueAttributes)
+    while queueAttributes['Attributes']['ApproximateNumberOfMessages'] > '0':
+        message = sqs.receive_message(
+        QueueUrl=SqsUrl,
+         MessageAttributeNames=[
+            'JobId'
+            ],
+        MaxNumberOfMessages=1,
+        VisibilityTimeout=20,
+        WaitTimeSeconds=5,
+        ReceiveRequestAttemptId='string'
+        )
+        messagejson = json.loads(message['Messages'][0]['Body'])
+        print(messagejson['JobStatus'])
+        #print(message['Messages'][0][])
+        continue
 
+SQSURL = 'https://sqs.us-east-2.amazonaws.com/256069468632/RekognitionQueue01'
 response = S3Exist('inputvideobucket', 'people-detection.mp4')
 if response != 'False':
   print('Key exists, continue ...')
@@ -86,7 +126,8 @@ if response != 'False':
   print(RekognitionStartResponse)
   JobSuccessChecker2(RekognitionStartResponse['JobId'], RekognitionNextToken)
   
-  if ResultsPaginater(RekognitionJobID=RekognitionStartResponse['JobId'], RekognitionNextToken=RekognitionNextToken) == True:
+  if RekognitionResultsPublisher(RekognitionJobID=RekognitionStartResponse['JobId'], RekognitionNextToken=RekognitionNextToken, SqsUrl=SQSURL) == True:
+      GetSqsMessages(SqsUrl=SQSURL, RekognitionJobID=RekognitionStartResponse['JobId'])
       print('Rekognition JSON response fetching complete ...')
   else: 
       print('failed to fetch Rekognition Job results ... ')
